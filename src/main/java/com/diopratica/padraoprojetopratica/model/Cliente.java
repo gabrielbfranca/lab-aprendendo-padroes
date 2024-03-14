50dbd7672b3015026482dc0d3eade1fdb07ecbb3
Gabriel Brito de França <gabrielbfranca@gmail.com>
2024-03-14 20:45:09 -0300

    Criação do projeto com mudança no projeto inicial como mudança de versão para 17, mudanças de build e  mudanças de documentação.

--- src/main/java/com/diopratica/padraoprojetopratica/model/Cliente.java
+++ src/main/java/com/diopratica/padraoprojetopratica/model/Cliente.java
@@ -18,6 +18,7 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String nome;
     @ManyToOne
     private Endereco endereco;
